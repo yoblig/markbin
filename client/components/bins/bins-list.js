ad291d7f5c9cc985120ccecf5df04c9dda87c500
yoblig <gilboyk@gmail.com>
2017-01-02 21:02:36 -0600

    added the bin component

--- client/components/bins/bins-list.js
+++ client/components/bins/bins-list.js
@@ -4,14 +4,34 @@ import { Bins } from '../../../lib/collections/bins';
 
 
 class BinsList extends Component {
+    onBinRemove(bin){
+        Meteor.call('bins.remove', bin);
+    }
+
+    renderList() {
+        return this.props.bins.map(bin => {
+            return (
+                <li className="list-group-item" key={bin._id}>
+                    Bin: {bin._id}
+                    <span className="pull-right">
+                        <button
+                            className="btn btn-danger"
+                            onClick={() => this.onBinRemove(bin)}>
+                            Remove
+                        </button>
+                    </span>
+                </li> 
+            );
+        });
+    }
     render(){
-        console.log(this.props.bins);
         return (
-            <div>Bins List</div>
+            <ul className="list-group">
+                {this.renderList()}
+            </ul>
         );
     }
 }
-
 
 export default createContainer(() => {
     Meteor.subscribe('bins');
--- client/main.js
+++ client/main.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from './components/app';
+import BinsMain from './components/bins/bins-main';
+import BinsList from './components/bins/bins-list';
 import { Bins } from '../lib/collections/bins';
 
+const routes = (
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <Route path="bins" component={BinsMain}/>
+    </Route>
+  </Router>
+);
+
 Meteor.startup(() => {
-  ReactDOM.render(<App/>, document.querySelector('.render-target'));
+  ReactDOM.render(routes, document.querySelector('.render-target'));
 });
