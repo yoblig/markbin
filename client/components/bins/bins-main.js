c58042750f4034fdcead30c650503b4806e0afab
yoblig <gilboyk@gmail.com>
2017-01-02 23:45:54 -0600

    Created container for binmain, subscribed to bins

--- client/components/bins/bins-main.js
+++ client/components/bins/bins-main.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Bins } from '../../../lib/collections/bins';
 
 class BinsMain extends Component {
     render(){
+        console.log(this.props.bin);
         return (
             <div>BINSMAIN</div>
         );
     }
 }
+// A container does data fetching and then renders its corresponding sub-component. That's it.
+export default createContainer((props) => {
+    const { binId } = props.params;
+    Meteor.subscribe('bins');
 
-export default BinsMain;
+    return { bin: Bins.findOne(binId) };
+}, BinsMain);
