13ff99aaeaf1268b791cc7fc43f978435bbacd68
yoblig <gilboyk@gmail.com>
2017-01-04 23:23:30 -0600

    Complete - Share feature implemented

--- client/components/bins/bins-list.js
+++ client/components/bins/bins-list.js
@@ -37,6 +37,7 @@ class BinsList extends Component {
 
 export default createContainer(() => {
     Meteor.subscribe('bins');
+    Meteor.subscribe('sharedBins');
 
     return { bins: Bins.find({}).fetch() };
 }, BinsList);
